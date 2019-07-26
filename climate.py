bf3f1b4e447b325346defd975439dac742160fbe
Ashley Schenker <47718194+aktavern@users.noreply.github.com>
2019-07-25 22:41:11 -0500

    remove extra dependency

--- climate.py
+++ climate.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import datetime as dt
-import re
 
 from flask import Flask, jsonify
 
